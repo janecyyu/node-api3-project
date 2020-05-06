9fbedf703e3ab450e9f9d56ab5e6350ff5ed3dca
janecyyu <janecyyu@gmail.com>
2020-05-06 12:25:18 -0700

    get / done

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
+const router = require('./users/userRouter');
 
 const server = express();
 
 server.use(logger);
-
-server.get("/", (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
-
+server.use("/",router);
+// server.get("/", (req, res) => {
+//   res.send(`<h2>Let's write some middleware!</h2>`);
+// });
 
 //custom middleware
 
@@ -18,6 +18,20 @@ function logger(req, res, next) {
     )}`
   );
   next();
+}
+
+function validateUserId(req,res,next){
+  let id = req.headers.id;
+  if (id && typeof id === "number") {
+    id = id.toLowerCase();
+    if (id === 1) {
+      next();
+    } else {
+      res.status(401).send("cannot pass!");
+    }
+  } else {
+    res.status(404).send("speak friend and enter");
+  }
 }
 
 module.exports = server;
